74010e73b50ba17657b380282c85ccd7ebd195a8
Anca Matei <ancamatei2012@gmail.com>
2024-06-25 22:21:52 +0300

    footer created fixed #2

--- src/App.js
+++ src/App.js
@@ -12,17 +12,20 @@ import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <div className="App">      
+      <div className='container'>
       <Navbar />
     
-      <Routes>
-        <Route path='' element={<Homepage />}> </Route>
-        <Route path='/authors' element={<Authors />}> </Route>
-        <Route path='/article-details/:id' element={<ArticleDetails />}> </Route>
-        <Route path='/admin' element={<Admin />}> </Route>
-        <Route path='*' element={<NotFound />}> </Route>
-      </Routes> 
-    
-      <ScrollToTop />
+    <Routes>
+      <Route path='' element={<Homepage />}> </Route>
+      <Route path='/authors' element={<Authors />}> </Route>
+      <Route path='/article-details/:id' element={<ArticleDetails />}> </Route>
+      <Route path='/admin' element={<Admin />}> </Route>
+      <Route path='*' element={<NotFound />}> </Route>
+    </Routes> 
+  
+    <ScrollToTop />
+      </div>
+      
       <Footer />
       
 
