a51aa23393c5a084bf51151f96c1462018dd8a03
Anca Matei <ancamatei2012@gmail.com>
2024-06-28 18:00:10 +0300

    homepage done, fixes #4

--- src/components/ArticleDetails.js
+++ src/components/ArticleDetails.js
@@ -1,5 +1,12 @@
+import { useLocation } from "react-router-dom";
 export default function ArticleDetails() {
+    const {state} = useLocation();
+    // console.log(state);
     return (
-        <>Article Details</>
+        // <>Article Details</>
+        <>
+        <h1>{state[0].title}</h1>
+        <p>by {state[1].name}</p>
+        </>
     );
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,25 +2,56 @@ import { fakeArticles, fakeAuthors } from "../fakeData";
 import Featured from './Featured';
 import Sidebar from './Sidebar'; 
 import ArticleCard from './ArticleCard';
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 export default function Homepage() {
+    const [searchParams] = useSearchParams(); 
+    const [paramAuthor, setParamAuthor] = useState();
+    let paramAuthorName = searchParams.get('author');
     const articles = fakeArticles; 
     const authors = fakeAuthors; 
+
     let featuredArticle = articles.find(article => article.featured===true); 
     let featuredArticleAuthor = authors.find(author => author.authorId === featuredArticle.authorId);
+
+    useEffect( () => {
+        setParamAuthor(authors.find(author => author.name === paramAuthorName));
+    }, [searchParams, authors, paramAuthorName])
+
     return (
         <div className='homepage container'>
-            <Link to={`/article-details/${featuredArticle.title}`} state={[featuredArticle, featuredArticleAuthor]}><Featured article = {featuredArticle} author = {featuredArticleAuthor}/></Link>
+            {   
+                paramAuthorName ? 
+                null
+                : 
+                <Link to={`/article-details/${featuredArticle.title}`} state={[featuredArticle, featuredArticleAuthor]}><Featured article = {featuredArticle} author = {featuredArticleAuthor}/></Link>
+            }
             <main>
                 <div className='article-cards-container'>
                     {
+                        paramAuthorName && paramAuthor ? 
+                        articles.filter( article => article.authorId === paramAuthor.authorId)
+                        .map(article =>{
+                            return (
+                                <Link to={`/article-details/${article.title}`} state={[article,paramAuthor]} key = {article.id}>
+                                    <ArticleCard article = {article} author={paramAuthor} />
+                                </Link> 
+                            )
+                        })
+                        :
                         articles.map(article => {
                             if(article.featured===false) {
                                 let author = authors.find(author => author.authorId === article.authorId)
-                                return <ArticleCard key = {article.id} article = {article} author={author} />
+                                return (
+                                <Link to={`/article-details/${article.title}`} state={[article,author]} key = {article.id}>
+                                    <ArticleCard article = {article} author={author} />
+                                </Link>
+                                )
                             } else return null; 
                         })
-                    }
+                    } 
+
                 </div>
                 <Sidebar authors = {authors}/>
             </main>
