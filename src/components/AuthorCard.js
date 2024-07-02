50878c22f48115484e75ba8e2bc305fd4c8d38f6
Anca Matei <ancamatei2012@gmail.com>
2024-07-02 10:23:18 +0300

    AuthorCard done, fixes #10

--- src/components/AuthorCard.js
+++ src/components/AuthorCard.js
@@ -1,5 +1,11 @@
-export default function AuthorCard() {
+export default function AuthorCard(props) {
     return (
-        <>Author Card</>
+        <div className="author-card">
+            <img src={props.author.image} alt={`${props.author.name}, one of the authors of the BlogApp`} width='80px' height='80px'></img>
+            <div>
+                <h3>{props.author.name}</h3>
+                <p>{props.author.bio}</p>
+            </div>
+        </div>
     );
 }
